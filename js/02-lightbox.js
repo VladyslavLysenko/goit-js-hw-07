cad32de627b846d16d11388ee07fff0579c27526
VladyslavLysenko <Dodog12362@gmail.com>
2022-09-25 20:17:02 +0300

    update

--- js/02-lightbox.js
+++ js/02-lightbox.js
@@ -1,4 +1,32 @@
 import { galleryItems } from './gallery-items.js';
 // Change code below this line
+// console.log(galleryItems);
 
-console.log(galleryItems);
+const gallery = document.querySelector(".gallery")
+// console.log(gallery);
+
+const markup = galleryItems.reduce((acc, item) =>
+acc + `<div class="gallery__item"><a class="gallery__item" href="${item.original}">
+  <img class="gallery__image" src="${item.preview}" alt="${item.description}" />
+</a></div>`, "")
+
+// console.log(markup);
+gallery.insertAdjacentHTML("beforeend", markup)
+
+gallery.addEventListener("click", onClick)
+
+function onClick(e) {
+  e.preventDefault()
+  if (e.target.classList.contains("gallery__image")) {
+    console.log(e.target);
+ 
+    var lightbox = new SimpleLightbox('.gallery a', {
+      captionSelector: "img",
+      captionsData: "alt",
+      captionDelay: 250,
+      // captionPositon: "bottom"
+      // caption: true,
+    });
+    
+  }
+}
